f1b9f2ece8b010785fb2b7c348e9c6b072d5107c
pan chen <pan.chen@ge1.iut-cachan.u-psud.fr>
2018-11-13 11:29:23 +0100

    J'ai fini exo 2 du DS

--- src/main.c
+++ src/main.c
@@ -2,9 +2,23 @@
 //
 
 #include "my_header.h"
+#include "math.h"
+
+#define ECART 1e-10
 
 int main(void)
 {
-    printf(toto)
+    double a =1.0,b=2.0,c=3.0;
+    double delta;
+    unsigned char nb_solution;
+
+    delta =b*b-4.0*a*c;
+    
+    if(_fabs(delta)<ECART)
+       nb_solution=1;
+    else if (delta>ECART)
+       nb_solution=2;
+    else
+       nb_solution=0;
 	return 0;
 }
