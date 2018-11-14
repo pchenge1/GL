08f5a9db6d2939c4f5c6e01a19146376f1ce83f2
pchenge1 <pan.chen@u-psud.fr>
2018-11-15 05:36:10 +0800

    i have done exo 45

--- src/main.c
+++ src/main.c
@@ -1,9 +1,32 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "my_header.h"
+#include "my_header.h"  
+#include "TP_lib.h"
 
+#define n=LancerDe()
 int main(void)
 {
-    return 0;
+  unsigned short int n;
+  unsigned long int s=0;
+
+  while(n!=1){
+  if(n%2==0){
+  s=s+n;
+ }
+  else if(n==3){
+  s=s*2;
+ }
+  else (n==5){
+    if(s>=2){
+    s=s-2;
+   }
+    else{
+    s=0;
+   }
+ }
+  
+
+
+return 0;
 }
