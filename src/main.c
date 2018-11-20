39c881d62572cc78f273e63d4f75654a97f9f254
pchenge1 <pan.chen@u-psud.fr>
2018-11-21 04:41:06 +0800

    il fault modifier

--- src/main.c
+++ src/main.c
@@ -7,22 +7,30 @@
 #define   SEUILB 100  
 int main(void)
 {
-    unsigned char bin[10]={0,1,2,3,4,5,6,7,8,9},i1,i2;
-	unsigned int  sat[10]=rand()%200-300;
-    for(i1=0;i1<10;i1=i1++)
+    unsigned char bin[10]={0,1,2,3,4,5,6,7,8,9},i1,i2,i3;
+	unsigned int  sat[10]=rand();
+   
+    for(i1=0;i1<10;i1=i1+1)
     {
-      if(bin[i]>seuil)
-      {
+      if(bin[i1]>seuil)
          bin[i1]=1; 
-      }
-      else
-      {						
+      else						
          bin[i1]=0;   
-      }
     }
     
-    for(i2=0;i2<10;i=i2++)
+    for(i2=0;i2<10;i2=i2+1)
 	{
-		if(sat[i2])>SEUILH
+		if(sat[i2]>SEUILH)
+        sat[i2]=SEUILH;
+        else if(sat[i2]<SEUILB)
+        sat[i2]=SEUILB;
+        else
+        sat[i2]=sat[i2];
+    }
+
+    for(i3=0;i3<10;i3=i3+1)
+    {
+
+    }
     return 0;					  
 }
