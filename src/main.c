4dcb7b76c6acbc886c5175ba93c2193a0e2d9a93
pan chen <pan.chen@ge1.iut-cachan.u-psud.fr>
2018-11-20 09:50:53 +0100

    exo-56

--- src/main.c
+++ src/main.c
@@ -2,8 +2,27 @@
 //
 
 #include "my_header.h"
-
+#define   seuil 3
+#define   SEUILH -100
+#define   SEUILB 100  
 int main(void)
 {
-    return 0;
+    unsigned char bin[10]={0,1,2,3,4,5,6,7,8,9},i1,i2;
+	unsigned int  sat[10]=rand()%200-300;
+    for(i1=0;i1<10;i1=i1++)
+    {
+      if(bin[i]>seuil)
+      {
+         bin[i1]=1; 
+      }
+      else
+      {						
+         bin[i1]=0;   
+      }
+    }
+    
+    for(i2=0;i2<10;i=i2++)
+	{
+		if(sat[i2])>SEUILH
+    return 0;					  
 }
