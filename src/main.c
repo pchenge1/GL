6a67f797c064d289ce5f3131351e18400640e921
pchenge1 <pan.chen@u-psud.fr>
2018-11-21 04:55:01 +0800

    il fault modifier

--- src/main.c
+++ src/main.c
@@ -4,11 +4,12 @@
 #include "my_header.h"
 #define   seuil 3
 #define   SEUILH -100
-#define   SEUILB 100  
+#define   SEUILB 200 
+#define   N 5 
 int main(void)
 {
-    unsigned char bin[10]={0,1,2,3,4,5,6,7,8,9},i1,i2,i3;
-	unsigned int  sat[10]=rand();
+    unsigned char bin[10]={0,1,2,3,4,5,6,7,8,9},i1,i2,i3,i4,i5;
+	unsigned int  sat[10]=rand(),dist[10],a[5]={1,5,9,8,11},b[5],miroir[5];
    
     for(i1=0;i1<10;i1=i1+1)
     {
@@ -30,7 +31,21 @@ int main(void)
 
     for(i3=0;i3<10;i3=i3+1)
     {
-
+        if(dist[i3]!=0)
+        dist[i3]=distance;
+        else
+        dist[i3]=0;
     }
+
+    for(i4=0;i4<10;i4=i4+1)
+    {
+      b[]=a[];
+    }
+
+    for(i5=0;i5<10;i5=i5+1)
+    {
+     miroir
+    }
+
     return 0;					  
 }
