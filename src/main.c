14bc8673aeb0c3d2f51bf1899dd302d2dae87ad2
pchenge1 <pan.chen@u-psud.fr>
2018-12-05 07:24:50 +0800

    i have tried my best

--- src/main.c
+++ src/main.c
@@ -7,21 +7,35 @@ int main(void)
 {
    char nom_fig[80],chaine[80];
    char phrase[80]="La figure pentagone_B possede 5 sommets avec cordonnees 10 20 30 20 30 40 43 53 35 65";
-   unsigned char nb_pts,i,j;
-   unsigned char x[5]={10,30,30,43,35},y[5]={20,20,40,53,65},a[10]; 
+   unsigned char nb_pts=rand()%5+3,i,j;
+   unsigned char x[5],y[5],a[10]; 
    sscanf(phrase"%*s %*s %s %*s %u %*s %*s %*s %u %u %u %u %u %u %u %u %u %u",nom_fig,%nb_pts,x[1],y[1],x[2],y[2],x[3],y[3],x[4],y[4],x[5],y[5]);
    if(nb_pts<5)
-   for(i=2*nb_pts,i<10,i++)
-   {a[10]=-1;}
-   for(j=0,j<10,j++)
    {
-   if(j%2==0)
-   x[i/2]=a[j];
-   else
-   
+      for(i=2*nb_pts,i<10,i++);
+      {
+       a[i]=-1;
+      }
    }
-   sprintf(chaine,"La figure %s possede %u somments",nom_fig,&nb_pts);
-   MessagePopup(pharse);
+   else
+   {
+
+   }
+   
+    for(j=0,j<10,j++);
+      {
+          if(j%2==0)
+          {
+          a[j]=x[j/2];
+          }
+          else
+          {
+          a[j]=y[j/2-1/2];
+          }
+       }
+   
+   sprintf(chaine,"La figure %s possede %u sommets",nom_fig,&nb_pts);
+   MessagePopup(phrase);
    return 0;
 }
  
