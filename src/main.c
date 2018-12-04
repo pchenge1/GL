b3c0d033096cbf5372d2ba44cc7438d01344b3fb
pan chen <pan.chen@ge1.iut-cachan.u-psud.fr>
2018-12-04 09:49:56 +0100

    i

--- src/main.c
+++ src/main.c
@@ -1,9 +1,28 @@
+#include <ansi_c.h>
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "my_header.h"
+//#include "my_header.h"
 
 int main(void)
 {
-    return 0;
+   
+    unsigned char nom[100],prenom[100];
+	unsigned int annee_de_naissance;
+	
+	printf("nom ");
+	scanf("%s",nom );
+	printf("prenom ");
+	scanf("%s",prenom );
+	printf("annee_de_naissance "); 
+	scanf("%u",&annee_de_naissance );
+	
+	printf("Je m'appelle %s %s et j'ai %u ans",nom,prenom,annee_de_naissance); 
+	
+	
+
+	
+
+	
+	return 0;
 }
