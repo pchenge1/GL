1a3bbedc388a5435079e6ffffe1004b078c40731
pan chen <pan.chen@ge1.iut-cachan.u-psud.fr>
2018-12-11 08:34:18 +0100

    e25

--- src/main.c
+++ src/main.c
@@ -3,7 +3,30 @@
 
 #include "my_header.h"
 
-int main(void)
-{
-    return 0;
-}
+#include <CVI_gui>
+#include <stdint.h>
+ 
+ void main (void)
+ {
+	uint8_t
+	unsigned char chaine[100];
+	unsigned long int valeur_resisitance;
+	chaine[0]='noir';
+	chaine[1]='brun';
+	chaine[2]='rouge';
+	chaine[3]='orange';
+	chaine[4]='jaune';
+	chaine[5]='vert';
+	chaine[6]='bleu';
+	chaine[7]='violet';
+	chaine[8]='gris';
+	chaine[9]='blanc';
+	chaine[1]='/0'; 
+	
+	
+	valeur_resisitance=(10*chaine+chaine)*10;
+ 	
+	
+	
+	return 0
+ }
