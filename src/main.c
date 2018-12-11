d506bd29721f5128b04c991bb0f1e582205e14f4
pan chen <pan.chen@ge1.iut-cachan.u-psud.fr>
2018-12-11 09:39:41 +0100

    i have finished

--- src/main.c
+++ src/main.c
@@ -2,30 +2,19 @@
 //
 
 #include "my_header.h"
-
-#include <CVI_gui>
-#include <stdint.h>
- 
+#include <math.h>
+#include <stdio.h> 
  void main (void)
  {
-	uint8_t
-	unsigned char chaine[100];
-	unsigned long int valeur_resisitance;
-	chaine[0]='noir';
-	chaine[1]='brun';
-	chaine[2]='rouge';
-	chaine[3]='orange';
-	chaine[4]='jaune';
-	chaine[5]='vert';
-	chaine[6]='bleu';
-	chaine[7]='violet';
-	chaine[8]='gris';
-	chaine[9]='blanc';
-	chaine[1]='/0'; 
+
+	unsigned int a,b,c;
+	int valeur_resisitance;
+	printf("please put in the numbers:");
+	scanf("%d %d %d", &a,&b,&c) ;
+	valeur_resisitance=(10*a+b)*pow(10,c);
+	printf("le valeur est %d ohm",valeur_resisitance) ;
 	
-	
-	valeur_resisitance=(10*chaine+chaine)*10;
- 	
+
 	
 	
 	return 0
